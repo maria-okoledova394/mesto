89ee831a7472f3b2d7d7dc0ce69bc599b31fd117
maria-okoledova394 <maria.okoledova@yandex.ru>
2020-12-16 03:34:13 +0300

    Did form

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
 let openButtonElement = document.querySelector('.profile__edit-button');
-let inputNameElement = document.querySelector('.popup__input_name');
-let subtitleElement = document.querySelector('.profile__subtitle');
-let inputDescriprionElement = document.querySelector('.popup__input_descriprion');
 let titleElement = document.querySelector('.profile__title');
+let subtitleElement = document.querySelector('.profile__subtitle');
 let popup = document.querySelector('.popup');
 let closeButtonElement = document.querySelector('.popup__close-button');
-let saveButtonElement = document.querySelector('.popup__button-save');
+let nameInput = document.querySelector('.popup__input_content_name');
+let jobInput = document.querySelector('.popup__input_content_job');
+let formElement = document.querySelector('.popup__container');
 
 function showPopup() {
     popup.classList.add('popup_opened');
-    inputNameElement.value = titleElement.textContent;
-    inputDescriprionElement.value = subtitleElement.textContent;
+    nameInput.value = titleElement.textContent;
+    jobInput.value = subtitleElement.textContent;
 }
 
 function hidePopup() {
     popup.classList.remove('popup_opened');
 }
 
-function saveInfo () {
-    popup.classList.remove('popup_opened');
-    titleElement.textContent = inputNameElement.value;
-    subtitleElement.textContent = inputDescriprionElement.value;
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    titleElement.textContent = nameInput.value;
+    subtitleElement.textContent = jobInput.value;
 }
 
+formElement.addEventListener('submit', handleFormSubmit); 
 openButtonElement.addEventListener('click', showPopup); 
-
-closeButtonElement.addEventListener('click', hidePopup); 
-
-saveButtonElement.addEventListener('click', saveInfo); 
-
-   document.onkeyup = function(e){
-    if(e){
-        var key = window.event ? e.keyCode : e.which;
-    }else{
-        var key = window.event ? event.keyCode : event.which;
-    }
-    if (key == '13') {
-        saveInfo();
-    }
-}
+closeButtonElement.addEventListener('click', hidePopup);
